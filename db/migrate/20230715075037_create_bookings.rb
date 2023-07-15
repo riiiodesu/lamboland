4a56f88f0590dc0574d20118fd74beaea9861369
Joseph Herbert <herbert.jr80@gmail.com>
2023-07-15 16:52:43 +0900

    booking model created

--- db/migrate/20230715075037_create_bookings.rb
+++ db/migrate/20230715075037_create_bookings.rb
@@ -4,8 +4,9 @@ class CreateBookings < ActiveRecord::Migration[7.0]
       t.datetime :start_date
       t.datetime :end_date
       t.integer :status
-      t.references :users
-      t.references :lambos
+      t.references :user, null: false, foreign_key: true
+      t.references :lambo, null: false, foreign_key: true
+
       t.timestamps
     end
   end
