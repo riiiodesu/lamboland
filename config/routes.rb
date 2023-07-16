24616f8e8d3f7cb28f4f586569e416495448d3f3
Joseph Herbert <herbert.jr80@gmail.com>
2023-07-16 18:28:05 +0900

    bookings controller done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'bookings/index'
+  get 'bookings/new'
+  get 'bookings/create'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :bookings, only: [:index, :new, :create]
   # Defines the root path route ("/")
+  root to: "bookings#index"
   root to: "home#index"
 end
