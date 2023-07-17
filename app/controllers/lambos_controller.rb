ac481ad5c584bda3407be47b3df494f63fd51e89
ryo nakano <oryom17r15@gmail.com>
2023-07-17 21:18:30 +0900

    create new create for lamno controller

--- app/controllers/lambos_controller.rb
+++ app/controllers/lambos_controller.rb
@@ -2,4 +2,25 @@ class LambosController < ApplicationController
   def index
     @lambos = Lambo.all
   end
+
+  def new
+    @lambo = Lambo.new
+  end
+
+  def create
+    @lambo = Lambo.new(lambo_params)
+    @lambo.user = @user
+    @lambo.save
+    redirect_to lambos_path(@user)
+  end
+
+  private
+
+  def lambo_params
+    params.require(:lambo).permit(:model, :year, :price, :description)
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 end
