0a703d8e78d15f012bf04b997b96c977010d15f8
Joseph Herbert <herbert.jr80@gmail.com>
2023-07-18 21:09:35 +0900

    modifications per Noemi's advice regarding the routes and the namespace setup

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,7 @@
 class BookingsController < ApplicationController
+
+
+
   def index
     @bookings = Booking.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :bookings, only: [:index, :new, :create]
+  resources :bookings, only: [:index, :update]
   # Defines the root path route ("/")
   root to: "pages#home"
-  resources :lambos
+  resources :lambos, only: [:index, :show, :new, :create] do
+    resources :bookings, only: [:new, :create]
+  end
+  namespace :owner do
+    resources :bookings, only: :index
+    resources :lambos, only: :index
+  end
 end
