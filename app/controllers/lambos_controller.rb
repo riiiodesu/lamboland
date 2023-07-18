7a9948422770f7799acef5c0829752668616fd25
ryo nakano <oryom17r15@gmail.com>
2023-07-18 09:02:23 +0900

    add show method and change on new and create

--- app/controllers/lambos_controller.rb
+++ app/controllers/lambos_controller.rb
@@ -1,17 +1,27 @@
 class LambosController < ApplicationController
+  # before_action :set_user, only: %i[new create]
+
   def index
     @lambos = Lambo.all
   end
 
   def new
     @lambo = Lambo.new
+    @current_user = current_user
   end
 
   def create
     @lambo = Lambo.new(lambo_params)
-    @lambo.user = @user
-    @lambo.save
-    redirect_to lambos_path(@user)
+    @lambo.user = current_user
+    if @lambo.save
+      redirect_to lambos_path(@user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @lambo = Lambo.find(current_user.id)
   end
 
   private
@@ -19,8 +29,8 @@ class LambosController < ApplicationController
   def lambo_params
     params.require(:lambo).permit(:model, :year, :price, :description)
   end
-
-  def set_user
-    @user = User.find(params[:user_id])
-  end
+    # not sure if below is required
+    # def set_user
+    #   @user = User.find(params[:user_id])
+    # end
 end
