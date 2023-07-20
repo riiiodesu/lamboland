0f31753a40c8d4e312dcfc27ebc787e67763a8ea
Joseph Herbert <herbert.jr80@gmail.com>
2023-07-20 21:59:18 +0900

    completed user journey for saturday

---
+++ app/policies/owner/booking_policy.rb
@@ -0,0 +1,11 @@
+class Owner::BookingPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+    def resolve
+      scope.where(user: user)
+    end
+  end
+end
