6ad5d63d6530c34efb820fd3581169eea2111ce0
ryo nakano <oryom17r15@gmail.com>
2023-07-29 11:21:19 +0900

    change

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,5 +4,3 @@ import "./controllers"
 import "bootstrap"
 // import HelloController from "hello"
 // application.register("hello", HelloController)
-
-<script src="https://kit.fontawesome.com/3c7387f890.js" crossorigin="anonymous"></script>
